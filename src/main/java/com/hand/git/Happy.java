b0dba6937108de7bb5e9c184ab3efb40ff01c0ac
test_glb <test_glb@t.com>
2019-04-09 14:37:23 +0800

    hot_zw

--- src/main/java/com/hand/git/Happy.java
+++ src/main/java/com/hand/git/Happy.java
@@ -3,13 +3,7 @@ package com.hand.git;
 public class Happy {
 	
 	public static void main(String[] args) {
-		System.out.println("111666");
-
-
-//8888
-		System.out.println("update zw");
-		//66666
-		System.out.println("update by zw");
+		System.out.println("branch hot_zw");
 	}
 
 }
