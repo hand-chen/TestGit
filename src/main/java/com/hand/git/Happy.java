be3072d90812dc986d09cbf1e7c0dae70d77af9b
test_glb <test_glb@t.com>
2019-04-09 15:19:33 +0800

    m1

--- src/main/java/com/hand/git/Happy.java
+++ src/main/java/com/hand/git/Happy.java
@@ -3,11 +3,7 @@ package com.hand.git;
 public class Happy {
 	
 	public static void main(String[] args) {
-
-		System.out.println("我是master 111");
-
-		System.out.println("我是hot_zw 111");
-
+		System.out.println("我是master 1");
 	}
 
 }
