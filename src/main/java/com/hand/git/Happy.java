b28ab186dc16b684b1cf235aaa7a785e75cf4ccf
test_glb <test_glb@t.com>
2019-04-09 14:20:45 +0800

    1

--- src/main/java/com/hand/git/Happy.java
+++ src/main/java/com/hand/git/Happy.java
@@ -5,8 +5,7 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("111666");
 
-		//8888
-		System.out.println("update by wangwei");
+
 
 		//66666
 		System.out.println("update by zw");
